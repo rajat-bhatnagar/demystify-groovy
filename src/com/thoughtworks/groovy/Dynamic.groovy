810a77ad9bab75e50afaee89a3a8e70b0d3f8686
rajatBhatnagar <rajat_bhatnagar@outlook.com>
2015-10-14 23:40:04 -0400

    Script to demonstrate the dynamic nature of groovy

---
+++ src/com/thoughtworks/groovy/Dynamic.groovy
@@ -0,0 +1,11 @@
+def dog = "bark"
+def seven = 7
+
+println dog
+println dog.getClass()
+println seven
+println seven.getClass()
+println dog.toUpperCase()
+seven = "eight"
+println seven.toUpperCase()
+println seven.getClass()
